0f7dab22004fbc25545cf67264475410dee20971
LilGonzz <lucasgonzzagas@gmail.com>
2022-04-15 22:46:06 -0300

    inicialização do projeto

---
+++ src/main/java/com/lilgonzz/prototypeecommerce/PrototypeEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.lilgonzz.prototypeecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrototypeEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrototypeEcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lilgonzz/prototypeecommerce/PrototypeEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lilgonzz.prototypeecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrototypeEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
